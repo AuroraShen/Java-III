41255b239d9d3a162662c0b519eede552698e651
AuroraShen <47667931+AuroraShen@users.noreply.github.com>
2020-06-16 16:23:48 -0500

    Update and rename DS_My.java to DataStructureADT.java

--- Implement DataStructureADT/DataStructureADT.java
+++ Implement DataStructureADT/DataStructureADT.java
@@ -1,35 +1,9 @@
-//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+/////////////////////////////// 80 COLUMNS WIDE /////////////////////////////// 
 //
-// Title:           Data Structure Program 1
-// Files:           p1
-// Course:          Comp Sci 400, LEC-004, Spring 2019
-//
+// Title:           Data Structure 
 // Author:          Ruoxi Shen
 // Email:           rshen2793@wisc.edu
 // Lecturer's Name: Andy Kuemmel
-// Due Date:        Before 10pm Thursday 2/7
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name:    n/a
-// Partner Email:   n/a
-// Partner Lecturer's Name: n/a
-// 
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-//   ___ Write-up states that pair programming is allowed for this assignment.
-//   ___ We have both read and understand the course Pair Programming Policy.
-//   ___ We have registered our team prior to the team registration deadline.
-//
-///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
-//
-// Students who get help from sources other than their partner must fully 
-// acknowledge and credit those sources of help here.  Instructors and TAs do 
-// not need to be credited here, but tutors, friends, relatives, room mates, 
-// strangers, and others do.  If you received no outside help from either type
-//  of source, then please explicitly indicate NONE.
-//
-// Persons:         (identify each person and describe their help in detail)
-// Online Sources:  (identify each URL and describe their assistance in detail)
-//
 /////////////////////////////// 80 COLUMNS WIDE /////////////////////////////// 
 	
 /**
